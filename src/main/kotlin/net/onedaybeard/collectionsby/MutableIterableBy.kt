6a56805792524d4dac480422f01bae3ee4d10276
Adrian Papari <a@onedaybeard.net>
2020-06-15 00:46:27 +0200

    add removeBy overload

--- src/main/kotlin/net/onedaybeard/collectionsby/MutableIterableBy.kt
+++ src/main/kotlin/net/onedaybeard/collectionsby/MutableIterableBy.kt
@@ -2,9 +2,11 @@ package net.onedaybeard.collectionsby
 
 
 fun <T, U> MutableIterable<T>.removeAllBy(selector: (T) -> U, match: U) =
-        removeAll(by(selector, match))
+        removeBy(selector, match)
 fun <T, U> MutableIterable<T>.removeAllBy(selector: (T) -> U, match: () -> U) =
-        removeAll(by(selector, match))
+        removeBy(selector, match)
 
 fun <T, U> MutableIterable<T>.removeBy(selector: (T) -> U, match: U) =
+        removeAll(by(selector, match))
+fun <T, U> MutableIterable<T>.removeBy(selector: (T) -> U, match: () -> U) =
         removeAll(by(selector, match))
